5ca1c67c1314a7cf38e3ec1634d64193aa0e7714
Ratneshkumar18 <ratnesh.coding@gmail.com>
2024-04-08 03:08:05 -0700

    Day-2

---
+++ Day-2/script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector(".menu");
+let lineTop = document.querySelector(".lineTop");
+let lineCenter = document.querySelector(".lineCenter");
+let lineLower = document.querySelector(".lineLower");
+
+menu.addEventListener("click", () => {
+    lineTop.classList.toggle("top");
+    lineCenter.classList.toggle("center");
+    lineLower.classList.toggle("lower")
+})
